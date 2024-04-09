b598db9761e18127d17bd8ca4650ca6441757541
jordan toujouse <jtoujo1@gmail.com>
2024-04-08 21:49:53 -0500

    correct my conditional statement.

--- src/app/UserProfile.jsx
+++ src/app/UserProfile.jsx
@@ -1,17 +1,21 @@
 export default function UserProfile({ userName, userBio, userHobbies }) {
-  {
-    !userName || userBio || userHobbies ? <h1>Missing User Info</h1> : null;
-  }
+  // {
+  //   !userName || userBio || userHobbies ? <h1>Missing User Info</h1> : null;
+  // }
 
   return (
     <div>
-      <h1>{userName}</h1>
-      <p>{userBio}</p>
-      <ul>
-        {userHobbies.map((hobby) => (
-          <li>{hobby}</li>
-        ))}
-      </ul>
+      <h1>{!userName ? "Missing User Info" : userName}</h1>
+      <p>{userBio ? userBio : "No Bio"}</p>
+      {!userHobbies.length ? (
+        <ul>
+          {userHobbies.map((hobby) => (
+            <li>{hobby}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No Hobbies</p>
+      )}
     </div>
   );
 }
